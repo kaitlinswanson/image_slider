d97e4ab175cacf92e33a8bc7370652e340400ae5
Kaitlin Swanson <kateswanson94@gmail.com>
2021-06-21 19:58:36 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//create image slider
+//automatically moves fwd every 5 seconds 
+//navigation circles at the bottom that can be clicked
+
+
+//set up a wide div which will contain images.
+//put it inside of a container div "picture frame"
+
+//build functions for "next" and "previous"
