83bbebbd9f10680fc795bba1666aef391e00306f
Kaitlin Swanson <kateswanson94@gmail.com>
2021-06-21 21:44:42 -0700

    Second Commit

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 //create image slider
+const image1 = document.getElementById('image1'); 
+const image2 = document.getElementById('image2'); 
+const image3 = document.getElementById('image3'); 
+
+//make change image function that puts images in an array 
+let imageArray = ['image1', 'image2', 'image3']; 
+
+function changeImage() { 
+    for(i = 0; i < imageArray.length; i++) {
+       console.log(imageArray[i])
+    } 
+}
+
+
+
+//setInterval(function () { 
+//changeImage();
+//}, 2000)
+
 //automatically moves fwd every 5 seconds 
+
+function showImage(name) {  
+    clearPrevious();
+    name.style.display="flex";
+}
+
+function clearPrevious() { 
+    image1.style.display = "none";
+    image2.style.display = "none";
+    image3.style.display = "none";
+}
+
+
 //navigation circles at the bottom that can be clicked
 
 
